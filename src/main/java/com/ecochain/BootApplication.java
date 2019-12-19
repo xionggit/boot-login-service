5b1e9a86d65c95f32e450599171477e02de8c54f
xiong <shaotianxiong@syxfjr.cn>
2019-12-19 15:27:02 +0800

    test cerdential
    Test Plan: T4
    
    Reviewers: admin
    
    Reviewed By: admin
    
    Subscribers: liuhaihua
    
    Differential Revision: http://192.168.10.219/D12

--- src/main/java/com/ecochain/BootApplication.java
+++ src/main/java/com/ecochain/BootApplication.java
@@ -16,11 +16,6 @@ public class BootApplication {
         SpringApplication.run(BootApplication.class, args);
 
         //Test node vimdiff-1
-		93b77bcc1f3b
-
-#         93b77bcc1f3b Merge branch 'test' of https://github.com/xionggit/boot-login-service into 
-#         d7eb7087fca6 Merge branch 'test' of https://github.com/xionggit/boot-login-service into test                                                                                                                                       
-#         386b250d4fe2 test nodepad□~Z~D  
 
     }
 }
