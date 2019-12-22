3411559bacf1c29a4513d89a6bacff07ec4b93ff
xiong <shaotianxiong@syxfjr.cn>
2019-12-22 11:52:47 +0800

    diff by admin
    Test Plan: T4
    
    Reviewers: stx
    
    Reviewed By: stx
    
    Differential Revision: http://192.168.10.219/D22

--- src/main/java/com/ecochain/BootApplication.java
+++ src/main/java/com/ecochain/BootApplication.java
@@ -15,7 +15,7 @@ public class BootApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
 
-        //Test node vimdiff-1 sf   arc diff --update D13  
+        //Test node vimdiff-1 sf   arc diff --update D13  -- diff by admin 
 
     }
 }
